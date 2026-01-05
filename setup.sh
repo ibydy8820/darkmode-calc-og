864e26d1cf098dc8c325478938871f7dfa2ba455
ibydy8820 <ibydy8820@gmail.com>
2026-01-05 12:55:59 +0300

    [OG AI] Добавлен setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Задача 1: Инициализация и создание проекта
+git init
+npm create vite@latest . -- --template react
+npm install
+
+# Задача 2: Структура папок
+mkdir -p src/components src/styles src/hooks src/utils
+
+# Задача 3: Установка линтеров
+npm install -D eslint prettier eslint-config-prettier eslint-plugin-react eslint-plugin-react-hooks
